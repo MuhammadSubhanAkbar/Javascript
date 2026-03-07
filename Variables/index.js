f5890bf9778d2888c872e71a955704926acd5947
Subhan Akbar <subhanakbar346@gmail.com>
2026-03-07 14:45:10 +0500

    Variables

--- Variables/index.js
+++ Variables/index.js
@@ -1,6 +1,15 @@
-// const button = document.getElementById("hello");
-//
-// button.addEventListener("click", function() {
-//     window.alert("Hello World!");
-// })
+// Variable = A container that store the value and behave like it is the value itself
+//Two variables cannot have same name
 
+//1. Declaration  let x;
+//2. Assigment x = 100
+
+//Declaration
+let x;
+x = 100;
+
+let age = 25;
+let price = 10.99;
+let gpa = 4.9;
+
+console.log(age, price, gpa, x)
